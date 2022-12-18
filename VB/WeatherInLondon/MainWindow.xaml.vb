3893829b33c8565c478994ca4f9ff7557f91eac9
DevExpressExampleBot <fakeEmail>
2022-12-18 15:36:56 +0400

    Source auto update [skip ci]

--- VB/WeatherInLondon/MainWindow.xaml.vb
+++ VB/WeatherInLondon/MainWindow.xaml.vb
@@ -1,38 +1,23 @@
-﻿Imports DevExpress.Charts.Designer
-Imports DevExpress.Xpf.Charts
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace WeatherInLondon
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 
+    Public Class TitleItem
 
-	Public Class TitleItem
-		Public Property Text() As String
-		Public Property ImageSource() As String
-	End Class
+        Public Property Text As String
+
+        Public Property ImageSource As String
+    End Class
 End Namespace
-
-
